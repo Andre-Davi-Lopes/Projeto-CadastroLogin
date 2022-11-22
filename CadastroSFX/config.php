80b1aa6d1e6477d39c33f7365f8620a144757153
André D. Lopes <111528571+Andre-Davi-Lopes@users.noreply.github.com>
2022-11-21 22:38:19 -0300

    projeto V2

---
+++ CadastroSFX/config.php
@@ -0,0 +1,14 @@
+<?php
+    
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbNmae = 'formulario-cadastro';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbNmae);
+
+    
+
+
+
+?>
